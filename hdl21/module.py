711ba61a6fbab0b2131324e0a421b28b55a4c634
Dan Fritchman <dan@fritch.mn>
2021-08-25 14:45:04 -0700

    Add `name` argument to `Module.add`. Add `Module.get`. Misc `Module` clean-up.

--- hdl21/module.py
+++ hdl21/module.py
@@ -1,20 +1,29 @@
 """
 # hdl21.module 
+The `Module` module (get it?)
 
-The `Module` Module (get it?)
 This module primarily defines the core circuit-reuse element `hdl21.Module`. 
 It also includes the fun machinery for class-syntax creation of `Module`s, 
-enabled by a custom metaclass and its partner custom dictionary, 
-which unwind `Module` sub-class-body contents in dataflow order. 
+particularly the `@module` (lower-case) decorator-function. 
 """
+
 import inspect
 from textwrap import dedent
-from typing import Any, Optional, List, Union, Dict
+from typing import Any, Optional, List, Union, Dict, get_args
 from pydantic.dataclasses import dataclass
 
 # Local imports
-from .instance import calls_instantiate
 from .signal import Signal, Visibility
+from .instance import calls_instantiate, Instance, InstArray
+from .interface import InterfaceInstance
+
+# Type-alias for HDL objects storable as `Module` attributes
+ModuleAttr = Union[Signal, Instance, InstArray, InterfaceInstance]
+
+
+def _is_module_attr(val: Any) -> bool:
+    """ Boolean indication of whether `val` is a valid `hdl21.Module` attribute. """
+    return isinstance(val, get_args(ModuleAttr))
 
 
 @calls_instantiate
@@ -42,13 +51,11 @@ class Module:
         self._importpath = None  # Optional field set by importers
         self._initialized = True
 
-    def __setattr__(self, key: str, val: object):
+    def __setattr__(self, key: str, val: Any) -> None:
         """ Set-attribute over-ride, organizing into type-based containers """
-        from .signal import Signal, Visibility
-        from .instance import Instance, InstArray
-        from .interface import InterfaceInstance
 
         if not getattr(self, "_initialized", False) or key.startswith("_"):
+            # Bootstrapping phase. Pass along to "regular" setattr.
             return super().__setattr__(key, val)
 
         # Protected attrs - the internal dicts
@@ -71,50 +78,51 @@ class Module:
         if key == "name":
             return super().__setattr__(key, val)
 
-        # Type-based organization
-        if isinstance(val, Signal):
-            val.name = key
-            self.namespace[key] = val
-            if val.vis == Visibility.PORT:
-                self.ports[key] = val
-            else:
-                self.signals[key] = val
-        elif isinstance(val, Instance):
-            val.name = key
-            self.instances[key] = val
-            self.namespace[key] = val
-        elif isinstance(val, InstArray):
-            val.name = key
-            self.instarrays[key] = val
-            self.namespace[key] = val
-        elif isinstance(val, InterfaceInstance):
-            val.name = key
-            self.interfaces[key] = val
-            self.namespace[key] = val
-        else:
+        if not _is_module_attr(val):
             raise TypeError(f"Invalid Module attribute {val} for {self}")
 
-    def add(self, val: Any) -> Any:
+        # Checks out! Name `val` and add it to our type-based containers.
+        val.name = key
+        _ = self._add(val)
+        return None
+
+    def add(self, val: ModuleAttr, *, name: Optional[str] = None) -> ModuleAttr:
         """ Add a named HDL object into one of our internal dictionaries.
 
         This allows for programmatic insertion of attributes whose names are not legal Python identifiers,
         such as keywords ('in', 'from') and those including invalid characters.
-        This method is also the means by which underscore-prefaced attributes are added during elaboration.
 
-        The added object `val` is also provided as the return value, enabling usages such as
-        `instance.inp = module.add(h.Input(width=5))` and similar. """
+        The added object `val` is also provided as the return value, enabling chaining-style usages such as
+        ```python
+        instance.inp = MyModule.add(h.Input(name="in", width=5))
+        ``` 
+        and similar. """
 
-        from .signal import Signal, Visibility
-        from .instance import Instance, InstArray
-        from .interface import InterfaceInstance
-
-        if not isinstance(val, (Signal, Instance, InstArray, InterfaceInstance)):
+        if not _is_module_attr(val):
             raise TypeError(f"Invalid Module attribute {val} for {self}")
-        if not val.name:
+
+        # Now sort out naming. We get two name-sources:
+        # (a) the function-argument `name` and (b) the value's `name` attribute.
+        # One or the other (and not both) must be set.
+        if name is None and val.name is None:
             raise RuntimeError(
                 f"Invalid anonymous attribute {val} cannot be added to Module {self.name}"
             )
-        # Type-based organization
+        if name is not None and val.name is not None:
+            raise RuntimeError(
+                f"Conflicting names {name} and {val.name} cannot be added to Module {self.name}"
+            )
+        if name is not None:
+            val.name = name
+
+        # Now `val.name` is set appropriately.
+        # Add it to our type-based containers, and return it.
+        return self._add(val)
+
+    def _add(self, val: ModuleAttr) -> ModuleAttr:
+        """ Internal `add` and `setattr` implementation. 
+        Primarily sort `val` into one of our type-based containers. """
+
         if isinstance(val, Signal):
             self.namespace[val.name] = val
             if val.vis == Visibility.PORT:
@@ -132,8 +140,16 @@ class Module:
             self.namespace[val.name] = val
         else:
             raise TypeError(f"Invalid Module attribute {val} for {self}")
+
         # And return our newly-added attribute
         return val
+
+    def get(self, name: str) -> Optional[ModuleAttr]:
+        """ Get module-attribute `name`. Returns `None` if not present. 
+        Note unlike Python built-ins such as `getattr`, `get` returns solely 
+        from the HDL namespace-worth of `ModuleAttr`s. """
+        ns = self.__getattribute__("namespace")
+        return ns.get(name, None)
 
     def __getattr__(self, key: str) -> Any:
         """ Include our namespace-worth of HDL objects in dot-access retrievals """
@@ -158,7 +174,7 @@ class Module:
         return f"Module(_anon_)"
 
     @property
-    def _interface_ports(self):
+    def _interface_ports(self) -> dict:
         """ Port-Exposed Interface Instances """
         return {name: intf for name, intf in self.interfaces.items() if intf.port}
 
@@ -178,7 +194,7 @@ def module(cls: type) -> Module:
     """
     # Module Definition Decorator
 
-    Converts a class-body full of Interface-storable attributes to an `hdl21.Module`.
+    Converts a class-body full of Module-storable attributes to an `hdl21.Module`.
     Example Usage:
 
     ```python
@@ -202,12 +218,11 @@ def module(cls: type) -> Module:
 
     Attempts to set module attributes of any other types will raise a `TypeError`.
     Notably this includes any behavioral elements implemented by Python functions.
-    If you'd like atribrary-executing Python-code that creates or manipulates `Modules`,
+    If you'd like arbitrary-executing Python-code that creates or manipulates `Modules`,
     you probably want an `hdl21.Generator` instead.
 
     Temporary variables can be declared inside of the `@module`-decorated class-body,
-    so long as they are meet the Python-standard convention for private data:
-    name-prefixing with an underscore.
+    so long as they are meet the Python convention for private data: name-prefixing with an underscore.
     These temporary variables are *not* propagated along as members of the `Module`.
     """
 
