4cb4f0070dc4ac4e85e599be45f89f000f0a5723
Dan Fritchman <dan@fritch.mn>
2023-05-31 23:21:29 +0000

    Add flattening tests for Generators, Slices, Concats

--- hdl21/tests/test_flatten.py
+++ hdl21/tests/test_flatten.py
@@ -111,3 +111,72 @@ def test_flatten_node_desc():
         str(nodes[0])
         == "{'name': 'inv_1:pmos', 'path': ['inv_1', 'pmos'], 'conns': {'d': 'inv_1_vout', 'g': 'vin', 's': 'vdd', 'b': 'vdd'}}"
     )
+
+
+def test_flatten_with_slices():
+    """Flatten a Module with slices"""
+
+    @h.module
+    class InvTwoPack:
+        # A two-bit-bus's worth of inverters
+        vdd, vss = h.Ports(2)
+        vin, vout = 2 * h.Port(width=2)
+        inv_0 = Inverter(vdd=vdd, vss=vss, vin=vin[0], vout=vout[0])  # type: ignore
+        inv_1 = Inverter(vdd=vdd, vss=vss, vin=vin[1], vout=vout[1])  # type: ignore
+
+    flattened = flatten(InvTwoPack)
+
+
+def test_flatten_with_concat():
+    """Test flattening a module with signal concatenations."""
+
+    @h.module
+    class NmosArray:
+        # An instance array of Nmos'es. Maybe for a current DAC?
+        g, vss = 2 * h.Port()
+        d = h.Port(width=8)
+        nmoses = 8 * h.Nmos()(g=g, d=d, s=vss, b=vss)
+
+    @h.module
+    class M:
+        # A thing that instantiates an NmosArray, and concatenates some signals together to form its drain connections.
+        g, vss = 2 * h.Port()
+        s4 = h.Signal(width=4)
+        s2 = h.Signal(width=2)
+        s1 = h.Signal(width=1)
+        s0 = h.Signal(width=1)
+        nmos_array = NmosArray(d=h.Concat(s4, s2, s1, s0), g=g, vss=vss)
+
+    flattened = flatten(M)
+
+
+def test_flatten_generator():
+    """Flatten a Generator
+    (And it has Concat too! Kinda. But not by the point flattening happens)."""
+
+    @h.paramclass
+    class Params:
+        how_many = h.Param(dtype=int, default=5, desc="How many inverters you want?")
+
+    @h.generator
+    def Gen(params: Params) -> h.Module:
+        if params.how_many < 2:
+            raise ValueError
+
+        @h.module
+        class M:
+            vdd, vss, vin, vout = h.Ports(4)
+            internal_signal = h.Signal(width=params.how_many - 1)
+            # Array of `how_many` series-connected inverters
+            invs = params.how_many * Inverter(
+                vdd=vdd,
+                vss=vss,
+                vin=h.Concat(vin, internal_signal),
+                vout=h.Concat(internal_signal, vout),
+            )
+
+        return M
+
+    gen5 = Gen(how_many=5)
+    assert not is_flat(gen5)
+    flattened = flatten(gen5)
