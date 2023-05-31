9e283ad160df137e4c4e91a66087eacc8ccd3858
ThomasPluck <tompluck@mail.com>
2023-05-31 19:53:33 +0100

    update vlsir dev version

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -7,9 +7,9 @@ pwd
 cd .. 
 git clone https://github.com/Vlsir/Vlsir.git
 cd Vlsir/bindings/python 
-pip install -e .
+pip install -e ".[dev]"
 cd ../../VlsirTools
-pip install -e .
+pip install -e ".[dev]"
 cd ../../Hdl21
 pip install -e ".[dev]"
 # pre-commit install
