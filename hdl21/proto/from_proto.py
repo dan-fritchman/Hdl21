f7a6fa65322f0f30a12017d7d84ef0d028636721
Dan Fritchman <dan@fritch.mn>
2023-06-28 13:12:12 -0700

    Add VLSIR Module.literals and test thereof

--- hdl21/proto/from_proto.py
+++ hdl21/proto/from_proto.py
@@ -128,9 +128,8 @@ class ProtoImporter:
                 inst.connect(pconn.portname, conn)
 
         # Import any literal content
-        # FIXME: https://github.com/dan-fritchman/Hdl21/issues/149
-        # for plit in pmod.literals:
-        #     module.literals.append(Literal(text=plit))
+        for plit in pmod.literals:
+            module.literals.append(Literal(text=plit))
 
         # Add the Module to our cache and return-namespace, and return it
         self.modules[pmod.name] = module
